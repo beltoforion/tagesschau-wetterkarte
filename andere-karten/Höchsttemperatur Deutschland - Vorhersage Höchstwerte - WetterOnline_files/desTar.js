5189090b0f27c35de7ae1d27641f12c2c0d6454a
Ingo Berg <github@beltoforion.de>
2026-05-23 12:02:24 +0200

    mehr karten hinzugefügt

---
+++ andere-karten/Höchsttemperatur Deutschland - Vorhersage Höchstwerte - WetterOnline_files/desTar.js
@@ -0,0 +1,3 @@
+ehrwald = "00305;25;so____";
+desTarWeather = "00305:so____";
+desTarTemp = "00305:25";
---
+++ andere-karten/Modellkarten für Deutschland (Temperatur 2m) _ Europa HD (5 Tage)_files/ads.js
@@ -0,0 +1,1 @@
+var e=document.createElement('div'); e.id='gSxn2hs93hjs2hAhDRb'; e.style.display='none'; document.body.appendChild(e);
